fec270d4f894c77dde522caaf1ae05dea00dfe6c
noamr200 <noamr200@gmail.com>
2020-12-28 13:41:37 +0200

    update actor

--- src/models/actor.js
+++ src/models/actor.js
@@ -1,7 +1,25 @@
 import { Component } from "react";
-
-class actor  extends Component
-{
-
+/*
+First name
+Last name
+Birthday
+Image
+IMDB link
+Age (a method that calculates the age based on the birthday)
+Create a class (not component) for an Actor.
+*/
+class actor {
+    constructor(_fname,_lname,_image,_IMDBlink,_birthDate)
+    {
+        this.fname=_fname;
+        this.lname=_lname;
+        this.image=_image;
+        this.IMDBlink=_IMDBlink;
+        this.birthDate=_birthDate;
+        this.age=this.getAge(_birthDate);
+    }
+    
+        getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
+    
 }
 export default actor;
