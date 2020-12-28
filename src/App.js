e9f3026bb7f46d13e7c3ea0f40c1caaa3605ae91
noamr200 <noamr200@gmail.com>
2020-12-28 14:31:05 +0200

    Actors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import actor from './models/actor';
+import Actor from './models/actor';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
 function App() {
-  let a=new actor("ff","dd","ff","vv","1980-11-11");
-  console.log(a);
+  let a1=new Actor("Brad ","Pitt","https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+  "https://www.imdb.com/name/nm0000093/","1963-12-18");
+  let a2=new Actor("Gal" ,"Gadot","https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY317_CR51,0,214,317_AL_.jpg",
+  "https://www.imdb.com/name/nm2933757/","1985-04-30");
+  let a3=new Actor(
+    "Daniel", "Day-Lewis", 
+    "https://m.media-amazon.com/images/M/MV5BMjE2NDY2NDc1Ml5BMl5BanBnXkFtZTcwNjAyMjkwOQ@@._V1_UY317_CR13,0,214,317_AL_.jpg", 
+    "https://www.imdb.com/name/nm0000358/?ref_=nv_sr_srsg_0#actor" ,"1957-04-29");
+  
+  let Actors={a1,a2,a3};
+  console.log(Actors);
   return (
     <div className="App">
       <header className="App-header">
--- src/models/actor.js
+++ src/models/actor.js
@@ -7,7 +7,7 @@ IMDB link
 Age (a method that calculates the age based on the birthday)
 Create a class (not component) for an Actor.
 */
-class actor {
+class Actor {
     constructor(_fname,_lname,_image,_IMDBlink,_birthDate)
     {
         this.fname=_fname;
@@ -21,4 +21,4 @@ class actor {
         getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
     
 }
-export default actor;
+export default Actor;
