fd7967379223657195a6601825a8062cda64eef7
noamr200 <noamr200@gmail.com>
2021-01-03 14:27:38 +0200

    Pages Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import CardArray from "./components/CardArray";
+import Home from "./pages/Home.js";
 import MovieCard from "./components/MovieCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import {Router} from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 function App() {
   const [movies,setMovies]=  React.useState([]);
   const [contents,setContents]=React.useState(null);
-  
+  const actorsAdress ="#/actors";
+  const moviesAdress ="#/movies";
+  const homeAdress ="#/";
+/*
+Create 3 pages and maps as following:
 
+"/" - home page. show a landing page for the app
+"/actors" - show the actors
+"/movies" - show movies (only if there is no connection between actor and movie)
+Add navbar to navigate between 3 options
+
+*/
 
  useEffect ( ()=> {
   axios.get("movies.json").then(res => { setMovies (res.data ); setContents(movies.map((Movie, index) => <MovieCard   key={index} movie={Movie}  />));
@@ -16,15 +32,42 @@ function App() {
 },[contents!=null]) ;
 console.log(contents);
   return (
+   
     <div className="App">
       <header className="App-header"> 
-      
-      <CardArray  />
-      <h1>Movies:</h1> 
-      <h1>Will be in a diffrent page soon!</h1>
-     {movies.length!=0? contents :"loading"}
+        
+         <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href={homeAdress}>Home</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href={homeAdress}>Home</Nav.Link>
+      <Nav.Link href={moviesAdress}>Movies</Nav.Link>
+      <Nav.Link href={actorsAdress}>Actors</Nav.Link>
+    </Nav>
+   
+  </Navbar>
+
+
+
+     
+      <div>
+      {/* If I render something before (or after) the router it will render always (in any route) */}
+      {/* <p>blabla</p> */}
+      <HashRouter>
+        <Switch>
+         <Route exact path="/"><Home/></Route>
+          <Route exact path="/actors"><CardArray/></Route>
+          <Route exact path="/movies">  {movies.length!=0? contents :"loading"} </Route>
+        </Switch>
+      </HashRouter>
+    </div>
+
+    
+     {/* <h1>Movies:</h1> */}
+    {/*  <h1>Will be in a diffrent page soon!</h1> */}
+   
       </header>
     </div>
+  
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+function Home(props)
+{
+    return (<div>
+        <iframe width="1200" height="1200" src="https://www.youtube.com/embed/-cvKiC2UkkQ" frameborder="0"  allow="accelerometer; autoplay;  clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>);
+}
+export default Home;
